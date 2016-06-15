8809a61992fe46b0051869f69518cbb8be31e581
Rainbow Yeung <laan.yeung@gmail.com>
2016-06-15 11:11:09 -0700

    final submit

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -2,10 +2,10 @@ package com.flatironschool.javacs;
 
 public class HelloWorld {
     public static Double getVersion() {
-      return Double.parseDouble(System.getProperty("java.version"));
+        return Double.parseDouble(System.getProperty("java.specification.version"));
     }
-
+    
     public static void main(String[] args) {
-	     System.out.println(getVersion());
+        System.out.println(HelloWorld.getVersion());
     }
 }
